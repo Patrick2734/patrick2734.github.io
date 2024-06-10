c2ff1cf2c5f7443a967f28ca8b5df852eb096314
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2024-06-10 13:55:32 +0200

    Create script.js

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+document.getElementById('zuccante-link').addEventListener('mouseover', function() {
+    document.getElementById('popup-zuccante').style.display = 'block';
+});
+
+document.getElementById('zuccante-link').addEventListener('mouseout', function() {
+    document.getElementById('popup-zuccante').style.display = 'none';
+});
