ca177c8253d7841fea5cdffdc361998b3a29bdc5
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2024-06-17 10:10:37 +0200

    Update script2.js

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,3 +1,30 @@
+
+/* ==========================================================================
+   jQuery plugin settings and other scripts
+   ========================================================================== */
+
+$(document).ready(function(){
+   // Sticky footer
+  var bumpIt = function() {
+      $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
+    },
+    didResize = false;
+
+  bumpIt();
+
+  $(window).resize(function() {
+    didResize = true;
+  });
+  setInterval(function() {
+    if (didResize) {
+      didResize = false;
+      bumpIt();
+    }
+  }, 250);
+  // FitVids init
+  $("#main").fitVids();
+
+  
 // init sticky sidebar
   $(".sticky").Stickyfill();
 
