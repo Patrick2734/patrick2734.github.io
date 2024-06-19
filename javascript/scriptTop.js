70928c80c9e73095edaa2b6dfa65e655b9cde03b
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2024-06-19 11:58:20 +0200

    Update scriptTop.js

--- javascript/scriptTop.js
+++ javascript/scriptTop.js
@@ -2,10 +2,8 @@
     $(this).scrollTop(0);
 });*/
 
-const scroller = document.querySelector("#scroller");
-const output = document.querySelector("#output");
-
-scroller.addEventListener("scroll", (event) => {
-  output.textContent = `scrollTop: ${scroller.scrollTop}`;
+//Reset scroll top
+history.scrollRestoration = "manual";
+$(window).on('beforeunload', function(){
+      $(window).scrollTop(0);
 });
-
