04eb3ed2cd98157b6e7c147ece878fc70aa04721
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2024-06-21 08:54:40 +0200

    Update scriptTop.js

--- javascript/scriptTop.js
+++ javascript/scriptTop.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $(this).scrollTop(0);
 });
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Attach the click event listener to the button
+document.querySelector('.return-to-top').addEventListener('click', scrollToTop);
