80bf19315c8911795c555d3a87cf6d75f582931a
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2024-06-24 08:53:06 +0200

    Create changeImg.js

---
+++ javascript/changeImg.js
@@ -0,0 +1,14 @@
+var images = [];
+images[0] = ['https://patrick2734.github.io/images/profile.jpg']; // Prima immagine
+images[1] = ['https://patrick2734.github.io/images/profile2.jpg']; // Seconda immagine
+var index = 0;
+function changeImage() {
+    document.getElementById("mainPhoto").src = images[index][0];
+    if (index == 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    setTimeout(changeImage, 5000); // Cambia l'immagine ogni 5 secondi
+}
+window.onload = changeImage;
