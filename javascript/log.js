7abae274476e8c8499cf62e459338bf1ecf3e152
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2025-04-08 09:18:50 +0200

    Update log.js

--- javascript/log.js
+++ javascript/log.js
@@ -14,7 +14,7 @@ fetch("https://api.ipify.org?format=json")
   .then(data => {
     const ip = data.ip;
     
-    if (ip !== "157.138.166.194") {
+    if (ip !== "157.138.166.194" and ip !== "94.247.10.125") {
       // Solo se l'IP NON è quello da escludere
       fetch("https://webhook.site/183eeec6-640b-4e5d-b9ff-bf1d5165adcc", {
         method: "POST",
