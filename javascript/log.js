5f79451ee9e3c823b6a63c3e6cbf50ebac46f8c8
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2025-04-08 09:16:24 +0200

    Update log.js

--- javascript/log.js
+++ javascript/log.js
@@ -1,4 +1,4 @@
-fetch("https://webhook.site/183eeec6-640b-4e5d-b9ff-bf1d5165adcc", {
+/*fetch("https://webhook.site/183eeec6-640b-4e5d-b9ff-bf1d5165adcc", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
@@ -7,4 +7,25 @@ fetch("https://webhook.site/183eeec6-640b-4e5d-b9ff-bf1d5165adcc", {
     page: window.location.href,
     userAgent: navigator.userAgent
   })
-});
+});*/
+// Funzione per ottenere l'IP del visitatore
+fetch("https://api.ipify.org?format=json")
+  .then(res => res.json())
+  .then(data => {
+    const ip = data.ip;
+    
+    if (ip !== "157.138.166.194") {
+      // Solo se l'IP NON è quello da escludere
+      fetch("https://webhook.site/183eeec6-640b-4e5d-b9ff-bf1d5165adcc", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          ip: ip,
+          page: window.location.href,
+          userAgent: navigator.userAgent
+        })
+      });
+    }
+  });
