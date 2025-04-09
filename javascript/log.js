5de43ccc6243ad1873a445aead1efe5e3fac973c
Patrick Fabbiani <patrick1fabbiani@gmail.com>
2025-04-09 10:42:57 +0200

    Update log.js

--- javascript/log.js
+++ javascript/log.js
@@ -21,7 +21,7 @@
     console.error('Error fetching IP or sending data:', error);
   });*/
 
-/*fetch("https://api.ipify.org?format=json")
+fetch("https://api.ipify.org?format=json")
   .then(res => res.json())
   .then(data => {
     const ip = data.ip;
@@ -39,37 +39,4 @@
   })
   .catch(error => {
     console.error('Error fetching IP or sending data:', error);
-  });*/
-
-function trackPageVisit() {
-  console.log("Page URL is:", window.location.href);
-  fetch("https://api.ipify.org?format=json")
-    .then(res => res.json())
-    .then(data => {
-      const ip = data.ip;
-      fetch("https://webhook.site/183eeec6-640b-4e5d-b9ff-bf1d5165adcc", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          ip: ip,
-          page: window.location.href,
-          userAgent: navigator.userAgent
-        })
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching IP or sending data:', error);
-    });
-}
-
-// Se è una SPA, rileva i cambiamenti di URL
-let lastUrl = location.href;
-new MutationObserver(() => {
-  const currentUrl = location.href;
-  if (currentUrl !== lastUrl) {
-    lastUrl = currentUrl;
-    trackPageVisit();
-  }
-}).observe(document, { subtree: true, childList: true });
+  });
